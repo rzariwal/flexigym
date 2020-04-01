02864aea64b6d6fef8fbbf034ddf182caf7718b5
Rohit Zariwal <rohitzariwal@rohits-mbp.lan>
2020-04-01 20:39:18 +0800

    Implementing connectivity with cloud sql

--- jwt-authentication/project/server/config.py
+++ jwt-authentication/project/server/config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 # postgres_local_base = 'postgresql://postgres:@localhost/'
 # database_name = 'flask_jwt_auth'
-postgres_local_base = 'postgresql+pg8000://postgres:postgres@127.0.0.1/'
+postgres_local_base = 'postgresql+pg8000://postgres:postgres@127.0.0.1:5432/'
 database_name = 'postgres-authentication'
 SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
 
