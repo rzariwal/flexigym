b0751199154e4f2ec12d268194e17d5ed897128b
danyjacob45 <danyjacob45@gmail.com>
2020-04-02 18:17:52 +0800

    [payment-api] add missing import

--- FlexiGYM-payment-service/payment-service/src/main/java/com/flexigym/paymentservice/PaymentServiceController.java
+++ FlexiGYM-payment-service/payment-service/src/main/java/com/flexigym/paymentservice/PaymentServiceController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
