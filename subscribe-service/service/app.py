cca4739fa868a9528d97eff8c23dbaacb43e81b7
danyjacob45 <danyjacob45@gmail.com>
2020-04-07 12:31:46 +0800

    [subscribe-api] code cleanup
    use cart.py instead

--- subscribe-service/service/app.py
+++ subscribe-service/service/app.py
@@ -9,30 +9,6 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route("/add")
-def addToOrder():
-    if 'email' not in session:
-        return redirect(url_for('loginForm'))
-    else:
-
-
-
-        productId = int(request.args.get('productId'))
-        with sqlite3.connect('database.db') as conn:
-            cur = conn.cursor()
-            cur.execute("SELECT userId FROM users WHERE email = '" + session['email'] + "'")
-            userId = cur.fetchone()[0]
-            try:
-                cur.execute("INSERT INTO kart (userId, productId) VALUES (?, ?)", (userId, productId))
-                conn.commit()
-                msg = "Added successfully"
-            except:
-                conn.rollback()
-                msg = "Error occured"
-        conn.close()
-        return redirect(url_for('root'))
-
-
 # from project setup reference
 app = setup.create_app()
 
