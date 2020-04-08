90bc16d8cf82bf2b965cdfcd180aa84e0629d990
Rohit Zariwal <rohitzariwal@rohits-mbp.lan>
2020-04-08 15:11:56 +0800

    Removing username/password for auth

--- jwt-authentication/project/server/config.py
+++ jwt-authentication/project/server/config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 # postgres_local_base = 'postgresql://postgres:@localhost/'
 # database_name = 'flask_jwt_auth'
-postgres_local_base = 'postgresql+pg8000//{}:{}@{}/'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'), os.getenv('DB_NAME'))
+postgres_local_base = 'postgresql+pg8000//{}:{}@localhost:5432/'.format(os.getenv('DB_USER'), os.getenv('DB_PASS'))
 database_name = 'postgres-authentication'
 SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
 
