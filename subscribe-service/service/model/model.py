212b579f04241c2c2ef7405d9372c31698b0a605
Dany Jacob <danyjacob45@gmail.com>
2020-04-09 20:19:44 +0800

    add to cart is more real now

--- subscribe-service/service/model/model.py
+++ subscribe-service/service/model/model.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, DateTime, func, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, func, Boolean, orm
 
 from sqlalchemy import create_engine
 
@@ -20,10 +20,9 @@ def create_tables(app):
 
 # represent each item -> should aligned with advertise-api
 class Item(db.Model):
-
     __tablename__ = 'item'
     item_id = Column(Integer, primary_key=True, autoincrement=True)
-    cart_id = Column(Integer, nullable = False)
+    cart_id = Column(Integer, nullable=False)
     package_id = Column(Integer, nullable=False)
     qty = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
@@ -35,63 +34,70 @@ class Item(db.Model):
 
     def updateCartId(self, id):
         self.cart_id = id
-       
+
     def to_json(self):
         return {
-            'item_id' : self.item_id,
-            'cart_id' : self.user_id,
-            'package_id' : self.package_id,
-            'qty' : self.qty,
-            'price' : self.price
+            'item_id': self.item_id,
+            'cart_id': self.user_id,
+            'package_id': self.package_id,
+            'qty': self.qty,
+            'price': self.price
         }
 
 
 # represent a shopping cart -> includes 1 or more items with varying quantities
 class ShoppingCart(db.Model):
-
     __tablename__ = 'shopping-cart'
     cart_id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(String, nullable=False)
-    cart_status = Column(db.String(20)) #OPEN --> CLOSED
+    cart_status = Column(db.String(20))  # OPEN --> CLOSED
     created_time = Column(DateTime(timezone=True), server_default=func.now())
     updated_time = Column(DateTime(timezone=True), server_default=func.now())
     total = Column(Integer)
     payment_status = Column(Boolean, default=False)
 
+
     def __init__(self, user_id):
-        self.content = dict() #content of cart
+        self.content = dict()  # content of cart
         self.user_id = user_id
         self.payment_status = False
         self.cart_status = "OPEN"
+
+    @orm.reconstructor
+    def init_on_load(self):
+        self.content = dict()  # content of cart
 
     def update(self, item):
         if item.package_id not in self.content:
             self.content.update({item.package_id: item})
             return
-        try:
-            self.content[item.package_id].qty = self.content[item.package_id].qty + item.qty
-        except AttributeError:
-            pass
+        else:
+            try:
+                self.content[item.package_id].qty = self.content[item.package_id].qty + item.qty
+            except AttributeError:
+                pass
+            return
 
     def get_total(self):
-        return sum([v.i_price * v.i_qty for _, v in self.content.items()])
+        return sum([v.price * v.qty for _, v in self.content.items()])
 
     def get_num_items(self):
         return sum([v.qty for _, v in self.content.items()])
 
     def remove_item(self, key):
         self.content.pop(key)
-        
+
     def to_json(self):
         return {
-            'cart_id' : self.cart_id,
-            'user_id' : self.user_id,
-            'cart_status' : self.cart_status,
-            'created_time' : self.created_time,
-            'updated_time' : self.updated_time,
-            'total' : self.total,
-            'payment_status' : self.payment_status
+            'cart_id': self.cart_id,
+            'user_id': self.user_id,
+            'cart_status': self.cart_status,
+            'created_time': self.created_time,
+            'updated_time': self.updated_time,
+            'total': self.total,
+            'payment_status': self.payment_status
         }
+
 
 class Product(object):
 
