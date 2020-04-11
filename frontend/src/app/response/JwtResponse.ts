e02c3bee3937a40b2aff412d54b441cf5ea35b83
Dany Jacob <danyjacob45@gmail.com>
2020-04-11 17:25:10 +0800

    angular frontend code

---
+++ frontend/src/app/response/JwtResponse.ts
@@ -0,0 +1,13 @@
+export class JwtResponse {
+    token: string;
+    type: string;
+    account: string;
+    name: string;
+    role: string;
+
+    auth_token: string;
+    message: string;
+    status: string;
+
+}
+
