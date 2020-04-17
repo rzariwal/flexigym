cbd17a622ce12e87496c39d1b08db7ccc724f080
Dany Jacob <danyjacob45@gmail.com>
2020-04-17 22:28:00 +0800

    some changes

--- web-front/src/app/service/advertise.service.ts
+++ web-front/src/app/service/advertise.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user';
-import { ProductComponent } from '../product/product.component';
 import { Product } from '../models/product';
 import { Observable } from 'rxjs';
 
