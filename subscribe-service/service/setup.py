877d379c3a5c5e53fa7b422006dc5cf58ca058bc
ehkaluwah <ehkalu@gmail.com>
2020-04-29 01:25:21 +0800

    Subsc :  update correct db name in subscribe service

--- subscribe-service/service/setup.py
+++ subscribe-service/service/setup.py
@@ -3,13 +3,15 @@ import os
 from flask import Flask
 from subscribe_api import subscribe_api_blueprint
 import model.model as model
+#from flask_cors import CORS
 
-SWAGGER_URL = '/service/docs'
-API_URL = '/service/subscribe/docs.json'
+SWAGGER_URL = '/api/docs'
+API_URL = '/api/subscribe/docs.json'
 
 
 def create_app():
     app = Flask(__name__)
+    # CORS(app)
 
     basedir = os.path.abspath(__file__)
 
@@ -17,8 +19,8 @@ def create_app():
         dict(
             SECRET_KEY="subscribe secretkey",
             WTF_CSRF_SECRET_KEY="subscribe csrf secret key",
-            #SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://root:test@flexigym-subscribe-service-db/subscribe',
-            SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(basedir) + 'flexigym-subscribe.db',
+            SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://root:test@flexigym-subscribe-api-db/subscribe',
+            # SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(basedir) + 'flexigym-subscribe.db',
             SQLALCHEMY_TRACK_MODIFICATIONS=False,
             JSON_SORT_KEYS=False
         )
@@ -28,4 +30,6 @@ def create_app():
     model.create_tables(app)
 
     app.register_blueprint(subscribe_api_blueprint)
+
+
     return app
