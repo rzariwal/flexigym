5da628c46cab0818850128d748218034f02d93b9
Dany Jacob <danyjacob45@gmail.com>
2020-05-06 16:44:44 +0800

    subsc: change url

--- subscribe-service/service/subscribe_api/subscribe.py
+++ subscribe-service/service/subscribe_api/subscribe.py
@@ -21,7 +21,7 @@ USER_API_OK = True
 USER_URL = "http://web:5000/packagesApi"
 
 PAYMENT_API_OK = True
-PAYMENT_URL = "http://34.107.247.50/payment"
+PAYMENT_URL = "http://payment-api:8000/payment"
 
 Session = sessionmaker(bind=engine)
 session = Session()
