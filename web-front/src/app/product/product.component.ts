931fbda930ea29c42c68862607d9ff3e1508618e
ehkaluwah <ehkalu@gmail.com>
2020-05-07 19:43:17 +0800

    angular : package add, edit, remove pages

--- web-front/src/app/product/product.component.ts
+++ web-front/src/app/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AdvertiseService } from '../service/advertise.service';
-import { SubscribeService } from '../service/subscribe.service';
+import { AuthService } from '../service/auth.service';
 import { User } from '../models/user';
 import { Product } from '../models/product';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-product',
@@ -13,10 +14,11 @@ import { Product } from '../models/product';
 export class ProductComponent implements OnInit {
   user: User;
   products: Product;
+  currentUserSubscription: Subscription;
 
   constructor(private router: Router,
               private advertiseService: AdvertiseService,
-              private subscribeService: SubscribeService) { }
+              private authService: AuthService) { }
 
   ngOnInit(): void {
     this.user = new User();
@@ -26,6 +28,22 @@ export class ProductComponent implements OnInit {
         console.log(this.products.packages);
       }
     );
+
+    this.currentUserSubscription = this.authService.currentUser.subscribe(user => {
+      console.log("User in user profile :" + JSON.stringify(user) );
+      //this.currentUser = user;
+      this.user = user;
+      //this.user.admin =true;
+    });
+
   }
 
+  remove(product : Product) {
+    this.advertiseService.delete(product).subscribe(prod => {
+        if (!prod) throw new Error;
+        this.router.navigate(['/']);
+      },
+      e => {
+      });
+  }
 }
--- web-front/src/app/service/advertise.service.ts
+++ web-front/src/app/service/advertise.service.ts
@@ -6,6 +6,7 @@ import {Observable, of} from 'rxjs';
 import {catchError, map} from 'rxjs/operators';
 import {advertiseApi} from '../../environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,7 +20,7 @@ export class AdvertiseService {
   }
 
   getAllPackages(user: User): Observable<any> {
-    console.log("this.advertiseUrl " + JSON.stringify(this.advertiseUrl) );
+    console.log("this.advertiseUrl " + JSON.stringify(this.advertiseUrl));
     let url = this.advertiseUrl + "/all"
     let body = JSON.stringify({"email": user.email, "password": user.password, "mobile": user.mobile});
     const options = {
@@ -44,5 +45,39 @@ export class AdvertiseService {
       })
     );
   }
+
+
+  update(productInfo: Product): Observable<Product> {
+    const url = `${this.advertiseUrl}/${productInfo.id}`;
+    let param = "?package_name=" + productInfo.package_name +
+      "&package_description=" + productInfo.package_description +
+      "&price=" + productInfo.price +
+      "&available_qty=" + productInfo.available_qty +
+      "&valid_from=2020-12-31 10:10:10" +
+      "&valid_to=2025-12-31 10:10:10" +
+      "&created_by=1"
+    ;
+    return this.http.put<Product>(url + param, null);
+  }
+
+  create(productInfo: Product): Observable<Product> {
+    const url = `${this.advertiseUrl}/all`;
+    let param = "?package_name=" + productInfo.package_name +
+      "&package_description=" + productInfo.package_description +
+      "&price=" + productInfo.price +
+      "&available_qty=" + productInfo.available_qty +
+      "&valid_from=2020-12-31 10:10:10" +
+      "&valid_to=2025-12-31 10:10:10" +
+      "&created_by=1"
+    ;
+    console.log(url + param);
+    return this.http.post<any>(url + param, null);
+  }
+
+  delete(productInfo: Product): Observable<any> {
+    const url = `${this.advertiseUrl}/${productInfo.id}`;
+    return this.http.delete(url);
+  }
+
 
 }
