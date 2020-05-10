c4f8b5095a5a2395e6aaead90ef765a5ee7c8066
ehkaluwah <ehkalu@gmail.com>
2020-05-10 13:41:19 +0800

    angular : add payment api in environment

--- web-front/src/environments/environment.prod.ts
+++ web-front/src/environments/environment.prod.ts
@@ -4,3 +4,4 @@ export const environment = {
 export const authApi = 'http://35.190.50.74/auth';
 export const advertiseApi = 'http://34.107.247.50/packagesApi';
 export const subscribeApi = 'http://35.190.50.74/subscribe';
+export const paymentApi = 'http://34.107.247.50/payment';
