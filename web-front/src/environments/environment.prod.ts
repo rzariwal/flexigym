c5f65a8cb350c7b34c466f6f3f8b558a06b84778
Rohit Zariwal <rohitzariwal@rohits-mbp.lan>
2020-05-25 10:30:51 +0800

    Changing IP: auth notif adver payment subsc angular

--- web-front/src/environments/environment.prod.ts
+++ web-front/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true
 };
-export const authApi = 'http://35.190.50.74/auth';
+export const authApi = 'http://34.69.67.237/auth';
 export const advertiseApi = 'http://34.107.247.50/packagesApi';
-export const subscribeApi = 'http://35.190.50.74/subscribe';
+export const subscribeApi = 'http://34.69.67.237/subscribe';
 export const paymentApi = 'http://34.107.247.50/payment';
