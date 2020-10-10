ac48e59b41e76843214f270c789ab3196ae4ff17
Dany Jacob <danyjacob45@gmail.com>
2020-10-10 17:11:16 +0800

    angular deploy

--- web-front/src/environments/environment.prod.ts
+++ web-front/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true
 };
-export const authApi = 'https://10.148.0.7/auth';
-export const advertiseApi = 'https://10.148.0.7/packagesApi';
-export const subscribeApi = 'https://10.148.0.7/subscribe';
-export const paymentApi = 'https://10.148.0.7/payment';
+export const authApi = 'https://flexigym.rohitzariwal.co.in/auth';
+export const advertiseApi = 'https://flexigym.rohitzariwal.co.in/packagesApi';
+export const subscribeApi = 'https://flexigym.rohitzariwal.co.in/subscribe';
+export const paymentApi = 'https://flexigym.rohitzariwal.co.in/payment';
