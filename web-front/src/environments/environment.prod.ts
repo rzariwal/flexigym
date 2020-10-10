6f663b36ec43001bbef9f9ccd2a8e68b63e47994
Dany Jacob <danyjacob45@gmail.com>
2020-10-10 17:50:26 +0800

    angular deploy with jwt URI change

--- web-front/src/environments/environment.prod.ts
+++ web-front/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true
 };
-export const authApi = 'https://flexigym.rohitzariwal.co.in/auth';
+export const authApi = 'http://web:5000/auth';
 export const advertiseApi = 'https://flexigym.rohitzariwal.co.in/packagesApi';
 export const subscribeApi = 'https://flexigym.rohitzariwal.co.in/subscribe';
 export const paymentApi = 'https://flexigym.rohitzariwal.co.in/payment';
