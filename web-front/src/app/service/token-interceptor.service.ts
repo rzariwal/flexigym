6b68a65d61643c1bf082218c054fca07c9919702
Dany Jacob <danyjacob45@gmail.com>
2020-10-18 17:36:27 +0800

    use actual apis: angular

--- web-front/src/app/service/token-interceptor.service.ts
+++ web-front/src/app/service/token-interceptor.service.ts
@@ -11,13 +11,18 @@ export class TokenInterceptorService implements HttpInterceptor {
   constructor(private authService: AuthService) { }
 
   intercept(req, next){
-    const token = this.authService.getJWTToken();
-    let tokenizedReq = req.clone({
-      setHeaders : {
-        Authorization: `Bearer ${token}`
-      }
+    const token = localStorage.getItem("id_token");
+    if(token) {
+      let tokenizedReq = req.clone({
+        setHeaders : {
+          Authorization: `Bearer ${token}`
+        }
 
-    })
-    return next.handle(tokenizedReq)
+      })
+      return next.handle(tokenizedReq)
+    }
+    else {
+      return next.handle(req);
+    }
   }
 }
--- web-front/src/environments/environment.ts
+++ web-front/src/environments/environment.ts
@@ -5,20 +5,11 @@
 export const environment = {
   production: false
 };
-// export const authApi = 'http://web:5000/auth';
-// export const advertiseApi = 'http://flexigym-advertise-service2:9100/';
-// export const subscribeApi = 'http://flexigym-subscribe-api:5000';
-// export const advertiseApi = 'http://192.168.99.100:5000/';
-// export const subscribeApi = 'http://192.168.99.100:6100';
-
 
 export const authApi = 'https://flexigym.rohitzariwal.co.in/auth';
-export const advertiseApi = 'http://34.69.67.237/packagesApi';
-export const subscribeApi = 'http://34.69.67.237/subscribe';
-export const paymentApi = 'http://34.69.67.237/payment';
-//export const paymentApi = 'http://34.69.67.237/payment/complete?paymentId=PAYID-L23V6BI6D575912N0249433H&token=EC-95P44507TV103620X&PayerID=DTZN8J2TMGQ3W';
-
-
+export const advertiseApi = 'https://flexigym.rohitzariwal.co.in/packagesApi';
+export const subscribeApi = 'https://flexigym.rohitzariwal.co.in/subscribe';
+export const paymentApi = 'https://flexigym.rohitzariwal.co.in/payment';
 
 /*
  * For easier debugging in development mode, you can import the following file
