7eba8df9e8cb6101cacafc38c960a82124ff1c43
ehkaluwah <ehkalu@gmail.com>
2020-11-01 06:37:34 +0800

    Register page input validation

--- web-front/src/app/app.module.ts
+++ web-front/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {FormsModule} from '@angular/forms';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -39,7 +39,8 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,    
+    ReactiveFormsModule
   ],
   providers: [
     AuthService,
--- web-front/src/app/register/register.component.ts
+++ web-front/src/app/register/register.component.ts
@@ -1,7 +1,65 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from "@angular/router";
+// import { AuthService } from '../service/auth.service';
+// import { User } from '../models/user';
+// import { FormControl, FormGroup, Validators } from "@angular/forms";
+
+// @Component({
+//   selector: 'app-register',
+//   templateUrl: './register.component.html',
+//   styleUrls: ['./register.component.css']
+// })
+// export class RegisterComponent implements OnInit {
+//   user: User;
+//   userForm: FormGroup;
+
+//   get email() { return this.userForm.get('email'); }
+//   get password() { return this.userForm.get('password'); }
+//   get mobile() { return this.userForm.get('mobile'); }
+
+//   constructor(private router: Router, private authService: AuthService) {
+//     this.user = new User();    
+//     this.user.active = false;
+//   }
+
+//   ngOnInit(): void {
+//     this.userForm = new FormGroup({
+//       email: new FormControl(this.user.email, [
+//         Validators.required,
+//         Validators.email,
+//         Validators.minLength(4)
+//       ]),
+//       password: new FormControl(this.user.password, [
+//         Validators.required,
+//         Validators.minLength(8)
+//       ]),
+//       mobile: new FormControl(this.user.mobile, Validators.required)
+//     });
+
+//   }
+
+//   onRegister() {
+//     //this.userService.register(this.user).subscribe(res => console.log('Done'));
+//     this.authService.register(this.user).
+//         subscribe(
+//           response => {
+//             console.log(response.auth_token);            
+//             this.user.active = true;
+//             this.router.navigate(['/']);
+//           },
+//           e => {
+//             console.log("error");
+//           }
+//         )
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router} from "@angular/router";
 import { AuthService } from '../service/auth.service';
 import { User } from '../models/user';
+import { FormControl, FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,29 +68,77 @@ import { User } from '../models/user';
 })
 export class RegisterComponent implements OnInit {
   user: User;
+  userForm: FormGroup;
 
-  constructor(private router: Router, private authService: AuthService) {
+  constructor(private router: Router, private authService: AuthService,private fb: FormBuilder) {
     this.user = new User();
     this.user.active = false;
+    this.createForm();
   }
+
 
   ngOnInit(): void {
+    // this.userForm = new FormGroup({
+    //     email: new FormControl(this.user.email, [
+    //       Validators.required,
+    //       Validators.email,
+    //       Validators.minLength(4)
+    //     ]),
+    //     password: new FormControl(this.user.password, [
+    //       Validators.required,
+    //       Validators.minLength(8)
+    //     ]),
+    //     mobile: new FormControl(this.user.mobile, Validators.required)
+    //   });
 
   }
+
+  get email() { return this.userForm.get('email'); }
+  get password() { return this.userForm.get('password'); }
+  get mobile() { return this.userForm.get('mobile'); }
 
   onRegister() {
     //this.userService.register(this.user).subscribe(res => console.log('Done'));
     this.authService.register(this.user).
         subscribe(
           response => {
-            console.log(response.auth_token);
-            console.log(response.auth_token);
-            this.user.active = true;
-            this.router.navigate(['/']);
+            console.log(JSON.stringify(response));   
+            if (response.status="fail")
+            {              
+              alert(response.message  );
+              this.router.navigate(['/']);
+            }
+            else{
+              console.log(response.auth_token);            
+              this.user.active = true;
+              this.router.navigate(['/']);
+            }            
           },
           e => {
-            console.log("error");
+            //var error = JSON.parse(e);
+            console.log("error" + JSON.stringify(e));
+            alert("Error " + e.error.message  );
           }
         )
+  }
+
+  createForm() {
+    this.userForm = this.fb.group({
+      email: new FormControl(this.user.email, [
+        Validators.required,
+        Validators.email,
+        Validators.minLength(4)
+      ]),
+      password: new FormControl(this.user.password, [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      mobile: new FormControl(this.user.mobile, Validators.required)
+    });
+  //   this.userForm = this.fb.group({
+  //     email: ['', Validators.required ],
+  //     password: ['', Validators.required ],
+  //     mobile: ['', Validators.required ],
+  //  });
   }
 }
